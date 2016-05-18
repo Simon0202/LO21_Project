e79ea8ce27dc46801a21119070ae6197633693aa
Simon LAURENT <simon.laurent@gmx.com>
2016-05-18 02:37:54 +0200

    signaux et slots

--- Calculatrice/mainwindow.cpp
+++ Calculatrice/mainwindow.cpp
@@ -6,10 +6,62 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //Taille des boutons spéciaux
+    ui->checkBoxClavier->setChecked(true);
+    ui->checkBoxBip->setChecked(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//*********
+//Clavier09
+//*********
+void MainWindow::on_pushButton0_released(){ui->lineEdit->insert("0");}
+void MainWindow::on_pushButton1_released(){ui->lineEdit->insert("1");}
+void MainWindow::on_pushButton2_released(){ui->lineEdit->insert("2");}
+void MainWindow::on_pushButton3_released(){ui->lineEdit->insert("3");}
+void MainWindow::on_pushButton4_released(){ui->lineEdit->insert("4");}
+void MainWindow::on_pushButton5_released(){ui->lineEdit->insert("5");}
+void MainWindow::on_pushButton6_released(){ui->lineEdit->insert("6");}
+void MainWindow::on_pushButton7_released(){ui->lineEdit->insert("7");}
+void MainWindow::on_pushButton8_released(){ui->lineEdit->insert("8");}
+void MainWindow::on_pushButton9_released(){ui->lineEdit->insert("9");}
+
+
+//********
+//LineEdit
+//********
+void MainWindow::on_pushBoutonDel_clicked()
+{
+    ui->lineEdit->backspace();
+}
+
+
+//**********
+//ClavierOpe
+//**********
+void MainWindow::on_pushButtonDiv1_released(){ui->lineEdit->insert("/");}
+void MainWindow::on_pushButtonMul_released(){ui->lineEdit->insert("*");}
+void MainWindow::on_pushButtonMoins_released(){ui->lineEdit->insert("-");}
+void MainWindow::on_pushButtonPlus_released(){ui->lineEdit->insert("+");}
+
+
+//******************************
+//Clear && Enter && Undo && Redo
+//******************************
+void MainWindow::on_pushButtonClear_released(){ui->lineEdit->clear();}
+void MainWindow::on_pushButtonEnter_released(){;}
+void MainWindow::on_pushButtonUndo_released(){ui->lineEdit->undo();}
+void MainWindow::on_pushButtonRedo_released(){ui->lineEdit->redo();}
+
+
+
+
+void MainWindow::on_checkBoxClavier_clicked()
+{
+  ui->vue4CalcClavier->setDisabled(ui->checkBoxClavier->isChecked());
+  ui->vue4CalcClavier->setEnabled(ui->checkBoxClavier->isChecked());
+
 }
--- Calculatrice/mainwindow.h
+++ Calculatrice/mainwindow.h
@@ -15,6 +15,49 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    //*********
+    //Clavier09
+    //*********
+    void on_pushButton0_released();
+    void on_pushButton1_released();
+    void on_pushButton2_released();
+    void on_pushButton3_released();
+    void on_pushButton4_released();
+    void on_pushButton5_released();
+    void on_pushButton6_released();
+    void on_pushButton7_released();
+    void on_pushButton8_released();
+    void on_pushButton9_released();
+
+
+    //********
+    //LineEdit
+    //********
+    void on_pushBoutonDel_clicked();
+
+
+    //**********
+    //ClavierOpe
+    //**********
+    void on_pushButtonDiv1_released();
+    void on_pushButtonPlus_released();
+    void on_pushButtonMoins_released();
+    void on_pushButtonMul_released();
+
+
+    //******************************
+    //Clear && Enter && Undo && Redo
+    //******************************
+    void on_pushButtonClear_released();
+    void on_pushButtonEnter_released();
+    void on_pushButtonUndo_released();
+    void on_pushButtonRedo_released();
+
+
+
+    void on_checkBoxClavier_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
